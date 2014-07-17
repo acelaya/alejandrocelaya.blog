98227f1c95d7b6c50aaab8cc10d25399f9855dcd
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-07-17 18:48:25 +0200

    Created deploy script

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Delete the old production folder
+rm - rf output_prod
+
+# Run pre-generate grunt tasks
+npm install
+grunt
+
+# Generate production site
+sculpin generate --env=prod
+
+# Undo change sin the layout made by grunt
+git checkout -- source/_views/default.html.twig
+
+# Run post-generate grunt tasks
+grunt postgenerate
+
+# Deploy blog
+
