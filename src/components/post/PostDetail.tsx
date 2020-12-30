175acf8daefe44de1758ea17f74e161cd8be006b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-12-30 23:44:28 +0100

    Added warning banner to posts older than 3 years

--- src/components/post/PostDetail.tsx
+++ src/components/post/PostDetail.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import { WithPostProps } from '../types';
 import { CarbonAds } from '../CarbonAds';
+import { OldPostBanner } from './OldPostBanner';
 import { PostHeading } from './PostHeading';
 import { PostContent } from './PostContent';
 
 export const PostDetail: FC<WithPostProps> = ({ post }) => (
   <div>
     <PostHeading post={post} />
+    <OldPostBanner post={post} />
     <CarbonAds />
     <PostContent post={post} />
   </div>
