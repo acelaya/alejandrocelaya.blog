8bb9641175d35d40cd507733cd11abd2d8918069
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-10-15 09:49:39 +0200

    Fixed post excerpt/summary generation

--- astro.config.ts
+++ astro.config.ts
@@ -3,20 +3,13 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { config } from './config/config';
-import inferDescription from 'rehype-infer-description-meta';
-import { rehypeMdxPluginPostSummary } from './plugins/rehype/mdx-post-summary-plugin.mjs';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://astro.build/config
 export default defineConfig({
   site: config.SITE_URL,
   outDir: './build',
-  integrations: [sitemap(), react(), mdx({
-    rehypePlugins: [
-      [inferDescription, { inferDescriptionHast: true, truncateSize: 300 }],
-      rehypeMdxPluginPostSummary,
-    ]
-  })],
+  integrations: [sitemap(), react(), mdx()],
 
   vite: {
     plugins: [
