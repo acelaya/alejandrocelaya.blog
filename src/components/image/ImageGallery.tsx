5168a75fe82aa675bd3e9d4476f0fc7a6b63491c
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-10-17 11:05:34 +0200

    Replace react-18-image-lightbox with react-photoswipe-gallery

--- src/components/image/ImageGallery.tsx
+++ src/components/image/ImageGallery.tsx
@@ -1,44 +1,41 @@
-import type { FC } from 'react';
-import { useMemo, useState } from 'react';
-import Lightbox from 'react-18-image-lightbox';
+import type { FC, MutableRefObject } from 'react';
 import type { ImageProps } from './Image';
 import { Image } from './Image';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 
-type Images = [ImageProps] | [ImageProps, ImageProps];
+type Dimensions = {
+  width?: number;
+  height?: number;
+};
 
-export const ImageGallery: FC<{ images: Images, modal?: boolean }> = ({ images, modal }) => {
-  const [isOpen, openModal] = useState<boolean>(false);
-  const [activeIndex, setActiveIndex] = useState<number>(0);
-  const nextIndex = useMemo(() => activeIndex + 1, [activeIndex]);
-  const prevIndex = useMemo(() => activeIndex - 1, [activeIndex]);
+type Image = ImageProps & Dimensions;
 
+type Images = [Image] | [Image, Image];
+
+type ImageGalleryProps = {
+  images: Images;
+  modal?: true;
+};
+
+export const ImageGallery: FC<ImageGalleryProps> = ({ images, modal }) => {
   return (
-    <>
+    <Gallery withCaption>
       <div className="row">
-        {images.map(({ footerText, url, size, centered }, index) => (
-          <Image
-            key={index}
-            url={url}
-            footerText={footerText}
-            size={size ?? (images.length === 1 ? 'big' : 'small')}
-            centered={centered ?? images.length === 1}
-            onClick={modal ? () => {
-              setActiveIndex(index);
-              openModal(true);
-            } : undefined}
-          />
+        {images.map(({ footerText, url, size, centered, ...dimensions }, index) => (
+          <Item key={index} original={url} thumbnail={url} caption={footerText} {...dimensions}>
+            {({ ref, open }) => (
+              <Image
+                ref={ref as MutableRefObject<HTMLDivElement>}
+                url={url}
+                footerText={footerText}
+                size={size ?? (images.length === 1 ? 'big' : 'small')}
+                centered={centered ?? images.length === 1}
+                onClick={modal && open}
+              />
+            )}
+          </Item>
         ))}
       </div>
-      {modal && isOpen && (
-        <Lightbox
-          mainSrc={images[activeIndex].url}
-          nextSrc={images[nextIndex]?.url}
-          prevSrc={images[prevIndex]?.url}
-          onCloseRequest={() => openModal(false)}
-          onMoveNextRequest={() => setActiveIndex((prev) => prev + 1)}
-          onMovePrevRequest={() => setActiveIndex((prev) => prev - 1)}
-        />
-      )}
-    </>
+    </Gallery>
   )
 }
