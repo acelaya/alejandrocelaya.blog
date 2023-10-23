3090ec999a82425952f0ec99aaffed88d1eb1c72
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-10-23 20:43:49 +0200

    Migrate footer to tailwind

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,84 +1,107 @@
 import { faGithub, faLinkedinIn, faXTwitter, faMastodon } from '@fortawesome/free-brands-svg-icons';
 import { faRss } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import type { FC } from 'react';
+import type { FC, PropsWithChildren } from 'react';
 import { ExternalLink } from 'react-external-link';
 import type { Categories, Post } from '../utils/posts';
 import { humanFriendlyCategory } from '../utils/posts';
 import Link from './Link';
+import { SecondaryContainer } from './SecondaryContainer.tsx';
+import { Container } from './Container.tsx';
 
 export type FooterProps = {
   latestPosts: Post[];
   categories: Categories;
 }
 
+const FooterList: FC<{ title: string; links: [string, string][] }> = ({ title, links }) => (
+  <>
+    <h3 className="font-normal mb-4">{title}</h3>
+    <ul className="space-y-3">
+      {links.map(([href, text]) => (
+        <li key={href} className="truncate">
+          <Link
+            href={href}
+            className="text-grey-light hover:text-grey-light hover:underline text-sm"
+          >
+            {text}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </>
+);
+
+const FollowMe = () => (
+  <>
+    <h3 className="font-normal mb-4">Follow me</h3>
+    <ul className="space-x-4">
+      <li className="inline text-md">
+        <Link className="text-grey-light" href="https://www.linkedin.com/in/alejandro-celaya-alastrue/">
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </Link>
+      </li>
+      <li className="inline text-md">
+        <Link className="text-grey-light" href="https://github.com/acelaya">
+          <FontAwesomeIcon icon={faGithub} />
+        </Link>
+      </li>
+      <li className="inline text-md">
+        <Link className="text-grey-light" href="https://mastodon.social/@acelaya" rel="me">
+          <FontAwesomeIcon icon={faMastodon} />
+        </Link>
+      </li>
+      <li className="inline text-md">
+        <Link className="text-grey-light" href="https://x.com/acelayaa">
+          <FontAwesomeIcon icon={faXTwitter} />
+        </Link>
+      </li>
+      <li className="inline text-md">
+        <ExternalLink className="text-grey-light" href="/atom.xml">
+          <FontAwesomeIcon icon={faRss} />
+        </ExternalLink>
+      </li>
+    </ul>
+  </>
+);
+
 const Footer: FC<FooterProps> = ({ latestPosts, categories }) => (
-  <footer id="fh5co-footer">
-    <div className="container">
-      <div className="row row-bottom-padded-md">
-        <div className="col-md-2 col-xs-12">
-          <div className="fh5co-footer-widget">
-            <h3>Categories</h3>
-            <ul className="fh5co-links">
-              {categories.map((category) => (
-                <li key={category}><Link href={`/category/${category}/`}>{humanFriendlyCategory(category)}</Link></li>
-              ))}
-            </ul>
-          </div>
+  <footer className="mt-4">
+    <SecondaryContainer>
+      <div className="flex flex-col md:flex-row space-y-6 md:space-x-4 md:space-y-0">
+        <div className="w-full md:w-1/6">
+          <FooterList
+            title="Categories"
+            links={categories.map((category) => [`/category/${category}/`, humanFriendlyCategory(category)])}
+          />
         </div>
 
-        <div className="col-md-7 col-xs-12">
-          <div className="fh5co-footer-widget">
-            <h3>Latest blog posts</h3>
-            <ul className="fh5co-links blog-posts">
-              {latestPosts.map((post) => <li key={post.slug}><Link href={post.url}>{post.data.title}</Link></li>)}
-            </ul>
-          </div>
+        <div className="w-full md:w-7/12">
+          <FooterList
+            title="Latest blog posts"
+            links={latestPosts.map((post) => [post.url, post.data.title])}
+          />
         </div>
 
-        <div className="col-md-3 col-xs-12">
-          <div className="fh5co-footer-widget">
-            <h3>Contents</h3>
-            <ul className="fh5co-links">
-              <li><Link href="/categories/">Categories</Link></li>
-              <li><Link href="/tags/">Tags</Link></li>
-            </ul>
-          </div>
+        <div className="w-full md:w-2/6">
+          <FooterList title="Contents" links={[['/categories/', 'Categories'], ['/tags/', 'Tags']]} />
 
-          <div className="fh5co-footer-widget" style={{ marginTop: '40px' }}>
-            <h3>Follow me</h3>
-            <ul className="fh5co-social">
-              <li>
-                <Link href="https://www.linkedin.com/in/alejandro-celaya-alastrue/">
-                  <FontAwesomeIcon icon={faLinkedinIn} />
-                </Link>
-              </li>
-              <li>
-                <Link href="https://github.com/acelaya"><FontAwesomeIcon icon={faGithub} /></Link>
-              </li>
-              <li>
-                <Link href="https://mastodon.social/@acelaya" rel="me"><FontAwesomeIcon icon={faMastodon} /></Link>
-              </li>
-              <li>
-                <Link href="https://x.com/acelayaa"><FontAwesomeIcon icon={faXTwitter} /></Link>
-              </li>
-              <li><ExternalLink href="/atom.xml"><FontAwesomeIcon icon={faRss} /></ExternalLink></li>
-            </ul>
+          <div className="mt-6 md:mt-10">
+            <FollowMe />
           </div>
         </div>
       </div>
-    </div>
+    </SecondaryContainer>
 
-    <div className="fh5co-copyright">
-      <div className="container">
-        <p className="pull-left">
-          <small>
-            &copy; {new Date().getFullYear()} <Link href="https://www.alejandrocelaya.com">Alejandro Celaya Alastrué</Link>.
-          </small>
-        </p>
-        <p className="pull-right"><small>Full-stack developer</small></p>
-      </div>
-    </div>
+    <Container className="py-8 flex flex-col md:flex-row items-center md:justify-between">
+      <p className="text-sm">
+        &copy;{`${new Date().getFullYear()} `}
+        <Link className="text-grey-light hover:text-grey-light hover:underline font-bold" href="https://www.alejandrocelaya.com">
+          Alejandro Celaya Alastrué
+        </Link>
+      </p>
+      <p className="text-sm">Full-stack developer</p>
+    </Container>
   </footer>
 );
 
