7fcb62195f7b1505c8fd19b896972bfb1a17f380
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-10-23 23:02:56 +0200

    Fixed a couple of styles

--- src/components/SecondaryContainer.tsx
+++ src/components/SecondaryContainer.tsx
@@ -1,8 +1,9 @@
+import classNames from 'classnames';
 import type { FC, PropsWithChildren } from 'react';
 import { Container } from './Container';
 
-export const SecondaryContainer: FC<PropsWithChildren> = ({ children }) => (
-  <div className="bg-gray-50 py-10">
+export const SecondaryContainer: FC<PropsWithChildren<{ className?: string }>> = ({ children, className }) => (
+  <div className={classNames('bg-gray-50 py-10', className)}>
     <Container>
       {children}
     </Container>
