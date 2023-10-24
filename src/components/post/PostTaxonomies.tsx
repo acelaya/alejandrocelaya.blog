e8326136af503c1accd2b3c86adf4c18ea5e5b6e
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-10-24 20:08:56 +0200

    Fix taxonomy label wrapping

--- src/components/post/PostTaxonomies.tsx
+++ src/components/post/PostTaxonomies.tsx
@@ -1,30 +1,24 @@
+import { clsx } from 'clsx';
 import type { FC } from 'react';
-import type { WithPostProps } from '../types';
+import type { TaxonomyType, WithPostProps } from '../types';
 import { PostTaxonomy } from './PostTaxonomy';
 
-const Categories: FC<{ categories: string[] }> = ({ categories }) => (
-  <ul className="flex space-x-1 justify-center">
-    {categories.map((category) => (
-      <li key={category}>
-        <PostTaxonomy type="category" value={category} />
-      </li>
-    ))}
-  </ul>
-)
-
-const Tags: FC<{ tags: string[] }> = ({ tags }) => (
-  <ul className="flex space-x-1 justify-center">
-    {tags.map((tag) => (
-      <li key={tag}>
-        <PostTaxonomy type="tag" value={tag} />
+const Taxonomies: FC<{ taxonomies: string[]; type: TaxonomyType }> = ({ taxonomies, type }) => (
+  <ul className={clsx('flex flex-wrap space-x-1 justify-center', {
+    'md:justify-end': type === 'tag',
+    'md:justify-start': type === 'category',
+  })}>
+    {taxonomies.map((taxonomy) => (
+      <li key={taxonomy}>
+        <PostTaxonomy type={type} value={taxonomy} />
       </li>
     ))}
   </ul>
 );
 
 export const PostTaxonomies: FC<WithPostProps> = ({ post }) => (
-  <div className="md:flex space-y-2 justify-between">
-    <Categories categories={post.data.categories} />
-    <Tags tags={post.data.tags} />
+  <div className="md:flex space-y-2 md:space-y-0 justify-between">
+    <Taxonomies type="category" taxonomies={post.data.categories} />
+    <Taxonomies type="tag" taxonomies={post.data.tags} />
   </div>
 )
