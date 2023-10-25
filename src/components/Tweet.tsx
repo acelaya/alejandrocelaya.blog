0b41d20e347676e50a3eefa6256554b6712fdb05
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-10-25 18:50:24 +0200

    Render tweets via astro plugin instead of react plugin

--- src/components/Tweet.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from 'react';
-import { Tweet as TweetEmbed } from 'react-tweet'
-
-export const Tweet: FC<{ tweetId: string }> = ({ tweetId }) => (
-  <div className="flex justify-center">
-    <TweetEmbed id={tweetId} />
-  </div>
-);
