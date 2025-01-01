f4c25066cd2ef20ffe6a49688b50a2845f06cc7d
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-01-01 11:35:12 +0100

    Make sure astro:content is not imported client-side

--- src/pages/atom.xml.ts
+++ src/pages/atom.xml.ts
@@ -1,7 +1,7 @@
 import rss from '@astrojs/rss';
 import { parseISO } from 'date-fns';
 import { config } from '../../config/config';
-import { getAllPosts } from '../utils/posts';
+import { getAllPosts } from '../utils/posts-content.ts';
 import { renderMarkdown } from '../utils/markdown';
 
 export async function GET() {
--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,7 +1,4 @@
-import { getCollection, z } from 'astro:content';
-import { format, parse } from 'date-fns';
-import { decode } from 'html-entities';
-import { renderMarkdown } from './markdown';
+import { z } from 'zod';
 
 export const PAGE_SIZE = 5;
 export const SUB_PAGE_SIZE = 10;
@@ -10,7 +7,9 @@ const categories = ['web', 'php', 'tools', 'oss'] as const;
 
 export type Categories = typeof categories;
 
-type CategoryType = Categories[number];
+export type CategoryType = Categories[number];
+
+export const getCategories = () => categories;
 
 const capitalize = <T extends string>(value: T): string => {
   const [firstLetter, ...rest] = value;
@@ -43,67 +42,3 @@ export interface Post {
 export type SimplePost = Pick<Post, 'body' | 'excerpt' | 'url'> & {
   title: string;
 }
-
-const postExcerpt = (body: string) => {
-  const excerpt = renderMarkdown(body)
-    .split('\n')
-    // Remove HTML/JSX tags
-    .flatMap((str) => str.replace(/<\/?[^>]+(>|$)/g, '').split('\n'))
-    // Decode HTML entities into their actual character
-    .map((str) => decode(str))
-    // Filter out MDX imports
-    .filter((line) => !line.startsWith('import '))
-    // Get only first 6 paragraphs
-    .slice(0, 6)
-    .join(' ')
-    // Truncate to 300 characters
-    .substring(0, 300);
-
-  return `${excerpt}…`;
-}
-
-export const getAllPosts = (): Promise<Post[]> => getCollection('posts').then(
-  (posts) => [...posts].reverse().map(
-    ({ slug, ...rest }) => {
-      const [year, month, day, ...restOfSlug] = slug.split('-');
-      const url = `/${year}/${month}/${day}/${restOfSlug.join('-')}/`;
-      const date = `${year}-${month}-${day}`;
-      const formattedDate = format(parse(date, 'y-M-d', new Date()), 'dd MMMM y');
-      const excerpt = postExcerpt(rest.body);
-
-      return {
-        slug,
-        url,
-        date,
-        formattedDate,
-        excerpt,
-        ...rest,
-      };
-    },
-  ),
-);
-
-export const getLatestPosts = async () => {
-  const posts = await getAllPosts();
-  return posts.slice(0, PAGE_SIZE);
-};
-
-export const getCategories = () => categories;
-
-export const getTags = async (): Promise<string[]> => {
-  const posts = await getAllPosts();
-  return [...new Set(posts.flatMap(({ data }) => data.tags))].sort();
-};
-
-export type PostFilter = { category: CategoryType } | { tag: string };
-
-export const filteredPosts = async (filter: PostFilter) => {
-  const posts = await getAllPosts();
-  return posts.filter((post) => {
-    if ('category' in filter) {
-      return post.data.categories.includes(filter.category);
-    }
-
-    return post.data.tags.includes(filter.tag);
-  });
-};
