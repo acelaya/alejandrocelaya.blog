8704555b1d30f65a030dabb15ef3fe62c93aea69
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-05-15 08:51:25 +0200

    Remove comments and disqus integration

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,4 @@
 export const config = {
-  DISQUS_SHORTNAME: 'acelayablog',
   SITE_URL: 'https://alejandrocelaya.blog',
   SITE_TITLE: 'Alejandro Celaya | Blog',
   SITE_SUBTITLE: 'Software development, agile methodologies and open source software'
--- src/components/post/PostCommentCount.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from 'react';
-import { CommentCount } from 'disqus-react';
-import { disqusPropsForPost } from '../../utils/disqus';
-import type { WithPostProps } from '../types';
-
-export const PostCommentCount: FC<WithPostProps> = ({ post }) => (
-  <CommentCount {...disqusPropsForPost(post.data.title, post.url)}>Comments</CommentCount>
-);
