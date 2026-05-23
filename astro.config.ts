5e149ca1d07592205a41051799cb0f9e861a56fd
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-05-23 09:50:44 +0200

    Remove astro-expressive-code

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@tailwindcss/vite';
-import expressiveCode from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 import { config } from './config/config';
 
@@ -10,7 +9,7 @@ import { config } from './config/config';
 export default defineConfig({
   site: config.SITE_URL,
   outDir: './build',
-  integrations: [sitemap(), react(), expressiveCode(), mdx()],
+  integrations: [sitemap(), react(), mdx()],
 
   vite: {
     plugins: [tailwind()],
